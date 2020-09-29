f7458470ad67801139367227192dbee2dd1cc4ba
Cyb3rghost <cyb3rghostx@gmail.com>
2020-09-29 12:01:11 +0400

    Bug foireux

--- preload.js
+++ preload.js
@@ -12,6 +12,75 @@ window.addEventListener('DOMContentLoaded', () => {
 
   window.$ = window.jQuery = require('jquery')
   require( 'datatables.net-bs4' )( $ );
+  const { Sequelize } = require('sequelize');
+
+  const sequelize = new Sequelize({
+      dialect: 'sqlite',
+      storage: 'FKSBDD.db'
+  });
+  
+  try {
+      sequelize.authenticate();
+      console.log('Connection has been established successfully.');
+  } catch (error) {
+      console.error('Unable to connect to the database:', error);
+  }
+
+  var Individus = sequelize.define('individus', {
+    username: {
+      type: Sequelize.STRING,
+      field: 'username' // Will result in an attribute that is firstName when user facing but first_name in the database
+    },
+    password: {
+      type: Sequelize.STRING
+    },
+    nom: {
+      type: Sequelize.STRING
+    },
+    prenom: {
+      type: Sequelize.STRING
+    },
+    fonction: {
+      type: Sequelize.STRING
+    },
+    role: {
+      type: Sequelize.STRING
+    },
+    created_at: {
+      type: Sequelize.STRING
+    },
+    updated_at: {
+      type: Sequelize.STRING
+    }
+  }, {
+    freezeTableName: true // Model tableName will be the same as the model name
+  });
+
+  Individus.findByPk(1).then(function (individu) {
+      console.log('TEST')
+  });
+
+  /*let Individus = sequelize.define('individus', {
+      username: Sequelize.STRING,
+      password: Sequelize.STRING,
+      nom: Sequelize.STRING,
+      prenom: Sequelize.STRING,
+      fonction: Sequelize.STRING,
+      role: Sequelize.STRING,
+  });
+
+  let individu = Individus.findAll({ raw: true });
+  console.log(individu);
+
+  sequelize.close();*/
+
+  /*Individus.findByPk(1).then((individus) => {
+      console.log(individus.get({ plain: true }));
+      console.log('********************')
+      console.log(`id: ${individus.id}, username: ${individus.username}`);
+  }).finally(() => {
+      sequelize.close();
+  });*/
 
   $('#menuInscription').attr("href", __dirname + '/inscription.html')
   $('#menuConnexion').attr("href", __dirname + '/index.html')
---
+++ script/UserController.js
@@ -0,0 +1,12 @@
+$('#connexion').submit(function(e) {
+
+    e.preventDefault();
+
+    var username = $('#username').val()
+    var password = $('#password').val()
+
+    //alert('Utilisateur : ' + username + ' / Password : ' + password)
+
+
+
+});
